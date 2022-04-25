23a0eb42e7d1a1e9a5f4b7e5278d5086815e2822
변기원 <bkw9603@naver.com>
2022-04-25 13:57:49 +0900

    지도, 검색결과 표시

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { ConnectedRouter } from "connected-react-router";
 import { Route } from "react-router";
 import { history } from "../src/redux/store";
 import MainPage from "./pages/MainPage";
+import Planning from "./pages/Planning"
 
 function App() {
   return (
     <>
       <ConnectedRouter history={history}>
         <Route path="/" exact component={MainPage} />
+        <Route path="/planning" component={Planning} />
       </ConnectedRouter>
     </>
   );
