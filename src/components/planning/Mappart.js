ac8f03313a4e54df627e5f2372420a2d9026c506
변기원 <bkw9603@naver.com>
2022-04-26 10:55:32 +0900

    장소검색기능 작업중

--- src/components/planning/Mappart.js
+++ src/components/planning/Mappart.js
@@ -1,50 +1,106 @@
 /* global kakao */
 import React, { useEffect } from "react";
+import styled from "styled-components";
 const { kakao } = window;
 
 const Mappart = () => {
   const [map, setMap] = React.useState();
-  const [visitPlace, setVisitPlace] = React.useState([]);
+  // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+  function zoomIn() {
+    map.setLevel(map.getLevel() - 1);
+  }
 
-  let ps = new kakao.maps.services.Places([map]);
-  let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-  let markers = [];
+  // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+  function zoomOut() {
+    map.setLevel(map.getLevel() + 1);
+  }
+
   useEffect(() => {
     let mapContainer = document.getElementById("map");
-    let mapOption = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-      level: 3,
+    let options = {
+      center: new window.kakao.maps.LatLng(37.5740339, 126.976775),
+      level: 4,
     };
-
-    let map = new kakao.maps.Map(mapContainer, mapOption);
+    let map = new window.kakao.maps.Map(mapContainer, options);
     setMap(map);
-    // 검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우를 생성합니다
   }, []);
-
-  // 키워드 검색을 요청하는 함수입니다
-  function searchPlaces(event) {
-    event.preventDefault();
+  //클릭이벤트 등록하는 곳
+  //   if (map) {
+  //     kakao.maps.event.addListener(map, "click", (mouseEvent) => {
+  //       const latlng = mouseEvent.latLng;
+  //       console.log(`위도는 ${latlng.getLat()},경도는 ${latlng.getLng()} `);
+  //     });
+  //   }
+  // 마커를 표시할 위치입니다
+  //   let position = new kakao.maps.LatLng(37.5740339, 126.976775);
+  //   let positions = [
+  //     {
+  //       content:
+  //         '    <div class="info">' +
+  //         '        <div class="title">' +
+  //         "            카카오 스페이스닷원" +
+  //         "        </div>" +
+  //         '            <div class="desc">' +
+  //         '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+  //         '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+  //         '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+  //         "            </div>" +
+  //         "    </div>",
+  //       latlng: new kakao.maps.LatLng(37.5735829, 126.982775),
+  //     },
+  //     {
+  //       content:
+  //         '    <div class="info">' +
+  //         '        <div class="title">' +
+  //         "            카카오 스페이스닷원" +
+  //         "        </div>" +
+  //         '            <div class="desc">' +
+  //         '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+  //         '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+  //         '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+  //         "            </div>" +
+  //         "    </div>",
+  //       latlng: new kakao.maps.LatLng(37.5740539, 126.976975),
+  //     },
+  //     {
+  //       content:
+  //         '    <div class="info">' +
+  //         '        <div class="title">' +
+  //         "            카카오 스페이스닷원" +
+  //         "        </div>" +
+  //         '            <div class="desc">' +
+  //         '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+  //         '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+  //         '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+  //         "            </div>" +
+  //         "    </div>",
+  //       latlng: new kakao.maps.LatLng(37.5741039, 126.978175),
+  //     },
+  //   ];
+  // 마커를 생성합니다
+  //   let marker = new kakao.maps.Marker({
+  //     position: position,
+  //     clickable: true,
+  //   });
+  // 마커를 지도에 표시합니다.
+  //   marker.setMap(map);
+  let markers = [];
+  let ps = new kakao.maps.services.Places();
+  let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+  const searchPlaces = () => {
     let keyword = document.getElementById("keyword").value;
-
     if (!keyword.replace(/^\s+|\s+$/g, "")) {
       alert("키워드를 입력해주세요!");
       return false;
     }
-
-    // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
     ps.keywordSearch(keyword, placesSearchCB);
-    document.getElementById("keyword").value = "";
-  }
-  // 장소검색이 완료됐을 때 호출되는 콜백함수 입니다
-  function placesSearchCB(data, status, pagination) {
+  };
+  //위에서 키워드로 장소검색이 완료되면 placesSearchCB 콜백함수 실행됨
+  const placesSearchCB = (data, status, pagination) => {
     if (status === kakao.maps.services.Status.OK) {
-      // 정상적으로 검색이 완료됐으면
-      // 검색 목록과 마커를 표출합니다
-      // data는 검색결과
-      console.log(data);
+      //검색이 정상적으로 완료되었으면 검색목록과 마커 표출
       displayPlaces(data);
-
-      // 페이지 번호를 표출합니다
+      //페이지 번호 표출
       displayPagination(pagination);
     } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
       alert("검색 결과가 존재하지 않습니다.");
@@ -53,220 +109,42 @@ const Mappart = () => {
       alert("검색 결과 중 오류가 발생했습니다.");
       return;
     }
-  }
-  // 검색 결과 목록과 마커를 표출하는 함수입니다
-  function displayPlaces(places) {
-    let listEl = document.getElementById("placesList"),
-      menuEl = document.getElementById("menu_wrap"),
-      fragment = document.createDocumentFragment(),
-      bounds = new kakao.maps.LatLngBounds(),
-      listStr = "";
+  };
 
-    // 검색 결과 목록에 추가된 항목들을 제거합니다
-    removeAllChildNods(listEl);
-
-    // 지도에 표시되고 있는 마커를 제거합니다
-    removeMarker();
-    for (let i = 0; i < places.length; i++) {
-      // 마커를 생성하고 지도에 표시합니다
-      let placePosition = new kakao.maps.LatLng(places[i].y, places[i].x),
-        marker = addMarker(placePosition, i),
-        itemEl = getListItem(i, places[i]); // 검색 결과 항목 Element를 생성합니다
-      // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-      // LatLngBounds 객체에 좌표를 추가합니다
-      bounds.extend(placePosition);
-
-      // 마커와 검색결과 항목에 mouseover 했을때
-      // 해당 장소에 인포윈도우에 장소명을 표시합니다
-      // mouseout 했을 때는 인포윈도우를 닫습니다
-
-      const onInfo = () => {
-        console.log(
-          `가게명: ${places[i].place_name}`,
-          `주소: ${places[i].address_name}`,
-          places[i].x,
-          places[i].y,
-          `도로명주소: ${places[i].road_address_name}`,
-          `url주소: ${places[i].place_url}`,
-          `매장번호: ${places[i].phone}`,
-          `위도: ${places[i].y}`,
-          `경도: ${places[i].x}`
-        ); //식당정보 전송
-      };
-
-      //   (function (marker, title, address_name, url, category) {
-      //     kakao.maps.event.addListener(marker, "click", function () {
-      //       displayInfowindow(marker, title, address_name, url, category);
-      //     });
-
-      //     // kakao.maps.event.addListener(marker, "mouseout", function () {
-      //     //   infowindow.close();
-      //     // });
-
-      //     kakao.maps.event.addListener(marker, "click", onInfo);
-      //   })(
-      //     marker,
-      //     places[i].place_name,
-      //     places[i].road_address_name,
-      //     places[i].place_url,
-      //     places[i].category_name
-      //   );
-
-      fragment.appendChild(itemEl);
-
-      var content =
-        '<div class="wrap">' +
-        '    <div class="info">' +
-        '        <div class="title">' +
-        "            카카오 스페이스닷원" +
-        '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
-        "        </div>" +
-        '        <div class="body">' +
-        '            <div class="img">' +
-        '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
-        "           </div>" +
-        '            <div class="desc">' +
-        '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
-        '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
-        '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
-        "            </div>" +
-        "        </div>" +
-        "    </div>" +
-        "</div>";
-      // 마커 위에 커스텀오버레이를 표시합니다
-      // 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
-      var overlay = new kakao.maps.CustomOverlay({
-        content: content,
-        map: map,
-        position: marker.getPosition(),
-      });
-
-      // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
-      kakao.maps.event.addListener(marker, "click", function () {
-        overlay.setMap(map);
-      });
-
-      // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
-      function closeOverlay() {
-        overlay.setMap(null);
-      }
-    }
-
-    // 검색결과 항목들을 검색결과 목록 Element에 추가합니다
-    listEl.appendChild(fragment);
-    menuEl.scrollTop = 0;
-
-    // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-    map.setBounds(bounds);
-  }
-
-  // 검색결과 항목을 Element로 반환하는 함수입니다
-  function getListItem(index, places) {
-    let el = document.createElement("li"),
-      itemStr =
-        '<span class="markerbg marker_' +
-        (index + 1) +
-        '"></span>' +
-        '<div class="info">' +
-        "   <h5>" +
-        places.place_name +
-        "</h5>";
-
-    if (places.road_address_name) {
-      itemStr +=
-        "    <span>" +
-        places.road_address_name +
-        "</span>" +
-        '   <span class="jibun gray">' +
-        places.address_name +
-        "</span>";
-    } else {
-      itemStr += "    <span>" + places.address_name + "</span>";
-    }
-
-    itemStr += '  <span class="tel">' + places.phone + "</span>" + "</div>";
-
-    el.innerHTML = itemStr;
-    el.className = "item";
-
-    return el;
-  }
-
-  // 마커를 생성하고 지도 위에 마커를 표시하는 함수입니다
-  function addMarker(position, idx, title) {
-    let imageSrc =
-        "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png", // 마커 이미지 url, 스프라이트 이미지를 씁니다
-      imageSize = new kakao.maps.Size(64, 69), // 마커 이미지의 크기
-      imgOptions = { offset: new kakao.maps.Point(27, 69) },
-      markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions),
-      marker = new kakao.maps.Marker({
-        position: position, // 마커의 위치
-        image: markerImage,
-      });
-
-    marker.setMap(map); // 지도 위에 마커를 표출합니다
-    markers.push(marker); // 배열에 생성된 마커를 추가합니다
-
-    return marker;
-  }
-
-  // 지도 위에 표시되고 있는 마커를 모두 제거합니다
-  function removeMarker() {
-    for (let i = 0; i < markers.length; i++) {
-      markers[i].setMap(null);
-    }
-    markers = [];
-  }
-
-  // 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
-  function displayPagination(pagination) {
-    let paginationEl = document.getElementById("pagination"),
-      fragment = document.createDocumentFragment(),
-      i;
-
-    // 기존에 추가된 페이지번호를 삭제합니다
-    while (paginationEl.hasChildNodes()) {
-      paginationEl.removeChild(paginationEl.lastChild);
-    }
-
-    for (i = 1; i <= pagination.last; i++) {
-      let el = document.createElement("a");
-      el.href = "#";
-      el.innerHTML = i;
-
-      if (i === pagination.current) {
-        el.className = "on";
-      } else {
-        el.onclick = (function (i) {
-          return function () {
-            pagination.gotoPage(i);
-          };
-        })(i);
-      }
-
-      fragment.appendChild(el);
-    }
-    paginationEl.appendChild(fragment);
-  }
-
-  // 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수입니다
-  // 인포윈도우에 장소명을 표시합니다
-
-  // 검색결과 목록의 자식 Element를 제거하는 함수입니다
-  function removeAllChildNods(el) {
-    while (el.hasChildNodes()) {
-      el.removeChild(el.lastChild);
-    }
-  }
-
+  const displayPlaces = () => {
+    let listEl = document.getElementById();
+  };
   return (
     <>
       <div id="map" style={{ width: "700px", height: "600px" }}></div>
-      <form>
-        <input id="keyword"></input>
-        <button onClick={searchPlaces}>검색하기</button>
-      </form>
-
+      <div id="menu_wrap" class="bg_white">
+        <div class="option">
+          <div>
+            <form onSubmit={searchPlaces}>
+              키워드 : <input type="text" id="keyword" size="15"></input>
+              <button type="submit">검색하기</button>
+            </form>
+          </div>
+        </div>
+        <ul id="placesList"></ul>
+        <div id="pagination"></div>
+      </div>
+      <ZoomBtndiv>
+        <button
+          onClick={() => {
+            zoomIn();
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            zoomOut();
+          }}
+        >
+          -
+        </button>
+      </ZoomBtndiv>
       <div style={{ listStyle: "none", padding: "20px" }}>
         <li id="placesList"></li>
         <li id="menu_wrap"></li>
@@ -275,5 +153,21 @@ const Mappart = () => {
     </>
   );
 };
+
+const ZoomBtndiv = styled.div`
+  position: relative;
+  bottom: 160px;
+  left: 660px;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  button {
+    background-color: red;
+    width: 30px;
+    height: 30px;
+    background-color: rgba(255, 255, 255, 0.7);
+    border: 1px solid black;
+  }
+`;
 
 export default Mappart;
